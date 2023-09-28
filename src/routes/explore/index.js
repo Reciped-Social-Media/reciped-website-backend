69af9cc26b12a69d550d98ade389ce56246a2f90
JoshuaRuthel <jruthel@syftanalytics.com>
2023-09-28 12:43:49 +0200

    Added Our Picks functionality

--- src/routes/explore/index.js
+++ src/routes/explore/index.js
@@ -1,16 +1,58 @@
 import express from "express";
 import { authenticateToken } from "../../middleware/index.js";
 import { Post, PostReview, Recipe, User } from "../../database/index.js";
+import { Op } from "sequelize";
 
 const router = express.Router();
 
+function generateRandomIds(count, maxId) {
+	const randomIds = [];
+	while (randomIds.length < count) {
+		const randomId = Math.floor(Math.random() * (maxId + 1));
+		if (!randomIds.includes(randomId)) {
+			randomIds.push(randomId);
+		}
+	}
+	return randomIds;
+}
+
 router.get("/", authenticateToken, async (req, res) => {
-	const userPosts = await Post.findAll({ include : [{ model: User }, { model: Recipe }, { model: PostReview }] });
-	const explorePosts = userPosts.map(post => {
+
+	const numberOfRecipesToFetch = 20;
+	const maxId = 2140392;
+	const randomIds = generateRandomIds(numberOfRecipesToFetch, maxId);
+
+	const random = await Recipe.findAll({
+		where: {
+			id: {
+				[Op.in]: randomIds,
+			},
+		},
+	});
+	const picks = random.map(rec => {
+		const { id, title } = rec.dataValues;
+		return {
+			recipeId: id,
+			title,
+			caption: "Give this a try!",
+			username: "Reciped",
+			rating: 3,
+			category: "All",
+		};
+	});
+
+	const userPosts = await Post.findAll({
+		include: [{ model: User }, { model: Recipe }, { model: PostReview }],
+	});
+
+	const posts = userPosts.map((post) => {
 		const { id, recipeId, caption, category, createdAt } = post.dataValues;
 		const reviews = post.dataValues.PostReviews;
-		const ratings = reviews.map(rev => rev.dataValues.rating);
-		const sum = ratings.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+		const ratings = reviews.map((rev) => rev.dataValues.rating);
+		const sum = ratings.reduce(
+			(accumulator, currentValue) => accumulator + currentValue,
+			0,
+		);
 		const rating = sum / ratings.length;
 		const username = post.dataValues.User.dataValues.username;
 		const title = post.dataValues.Recipe.dataValues.title;
@@ -26,7 +68,7 @@ router.get("/", authenticateToken, async (req, res) => {
 		};
 	});
 
-	res.send({ post: explorePosts });
+	res.send({ posts, picks });
 });
 
 export default router;
