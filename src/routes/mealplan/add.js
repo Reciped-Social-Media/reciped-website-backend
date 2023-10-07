95785beff20d6f6df9445b51f8f8fc57f53a42ab
Xirynx <64374129+Xirynx@users.noreply.github.com>
2023-10-07 20:42:04 +0200

    Overhaul

--- src/routes/mealplan/add.js
+++ src/routes/mealplan/add.js
@@ -1,26 +1,53 @@
 import express from "express";
-import { authenticateToken } from "../../middleware/authenticateToken.js";
+import { authenticateToken } from "../../middleware/index.js";
 import { UserMealPlan } from "../../database/index.js";
 
 const router = express.Router();
 
 router.post("/", authenticateToken, async (req, res) => {
-	const { userId, recipeId, date, time, source } = req.body;
+	const { userId, recipeId, dateEpochMs, time, source } = req.body;
 	console.log(req.body);
 
-	const response = await UserMealPlan.findOne({ where: { userId, date, time } });
-	console.log("RES", response);
-	if (response) {
-		res.send({ error: "Already have a recipe in this timeslot" });
+	if (
+		!recipeId || typeof recipeId !== "number" ||
+		!date || typeof date !== "number" ||
+		!time || ["Breakfast", "Lunch", "Dinner", "Dessert"].includes(time) ||
+		!source || ["Cookbook", "Planner"].includes(source)
+	) {
+		res.status(400).send({ error: "Invalid format" });
 		return;
 	}
-	const addedMeal = await UserMealPlan.findOrCreate({ where: { userId, recipeId, date, time, source } })
+
+	let dateExact;
+	try {
+		dateExact = new Date(dateEpochMs);
+	}
+	catch {
+		res.status(400).send({ error: "Invalid format" });
+		return;
+	}
+
+	const date = new Date(dateExact.getFullYear(), dateExact.getMonth(), dateExact.getDate());
+
+	const exists = await UserMealPlan.findOne({ where: { userId, recipeId, date } })
 		.catch(err => {
 			console.error(err);
-			res.send({ error: "Something went wrong!" });
+			res.status(500).send({ error: "Something went wrong!" });
+			return "error";
 		});
+	if (exists === "error") return;
+	if (exists && exists !== "error") {
+		res.status(409).send({ error: "Meal already exists!" });
+		return;
+	}
 
-	if (!addedMeal) return;
+	const meal = await UserMealPlan.create({ userId, recipeId, date, time, source })
+		.catch(err => {
+			console.error(err);
+			res.status(500).send({ error: "Something went wrong!" });
+		});
+	if (!meal) return;
+
 	res.sendStatus(200);
 });
 
