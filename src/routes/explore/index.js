ab8b44d3c4623217e6bda36839c25229036ff3cb
Xirynx <64374129+Xirynx@users.noreply.github.com>
2023-10-17 00:44:41 +0200

    Update endpoint

--- src/routes/explore/index.js
+++ src/routes/explore/index.js
@@ -1,74 +1,177 @@
 import express from "express";
 import { authenticateToken } from "../../middleware/index.js";
-import { Post, PostReview, Recipe, User } from "../../database/index.js";
-import { Op } from "sequelize";
+import { Post, PostLike, PostReview, Recipe, User } from "../../database/index.js";
+import { Op, col } from "sequelize";
 
 const router = express.Router();
 
-function generateRandomIds(count, maxId) {
-	const randomIds = [];
-	while (randomIds.length < count) {
-		const randomId = Math.floor(Math.random() * (maxId + 1));
-		if (!randomIds.includes(randomId)) {
-			randomIds.push(randomId);
-		}
-	}
-	return randomIds;
+async function findPostById(postId, userId) {
+	const post = await Post.findOne({ where: { id: postId }, include: [{ model: Recipe }, { model: User }, { model: PostLike }, { model: PostReview }] })
+		.catch(err => {
+			console.log(err);
+			return null;
+		});
+	if (!post) return null;
+	return {
+		id: post.dataValues.id,
+		user: {
+			id: post.dataValues.User.dataValues.id,
+			username: post.dataValues.User.dataValues.username,
+		},
+		recipe: {
+			id: post.dataValues.Recipe.dataValues.id,
+			title: post.dataValues.Recipe.dataValues.title,
+			ingredients: post.dataValues.Recipe.dataValues.ingredients,
+			directions: post.dataValues.Recipe.dataValues.directions,
+		},
+		caption: post.dataValues.caption,
+		category: post.dataValues.category,
+		likes: post.dataValues.PostLikes.length,
+		liked: post.dataValues.PostLikes.map(like => like.dataValues.userId).includes(Number(userId)),
+		reviews: post.dataValues.PostReviews.length,
+		rating: post.dataValues.PostReviews.length > 0 ? Math.round(post.dataValues.PostReviews.reduce((acc, cur) => acc + cur.dataValues.rating * 2, 0) / post.dataValues.PostReviews.length) / 2 : 0,
+	};
+}
+
+
+async function findPosts(searcherId, options) {
+	const fullOptions = {
+		...options,
+		include: [{ model: Recipe }, { model: User }, { model: PostLike }, { model: PostReview }],
+	};
+	const postsRaw = await Post.findAll(fullOptions).catch(err => {
+		console.log(err);
+		return [];
+	});
+	if (!postsRaw) return [];
+
+	const posts = postsRaw.map(post => {
+		return {
+			id: post.dataValues.id,
+			user: {
+				id: post.dataValues.User.dataValues.id,
+				username: post.dataValues.User.dataValues.username,
+			},
+			recipe: {
+				id: post.dataValues.Recipe.dataValues.id,
+				title: post.dataValues.Recipe.dataValues.title,
+				ingredients: post.dataValues.Recipe.dataValues.ingredients,
+				directions: post.dataValues.Recipe.dataValues.directions,
+			},
+			caption: post.dataValues.caption,
+			category: post.dataValues.category,
+			likes: post.dataValues.PostLikes.length,
+			liked: post.dataValues.PostLikes.map(like => like.dataValues.userId).includes(Number(searcherId)),
+			reviews: post.dataValues.PostReviews.length,
+			rating: post.dataValues.PostReviews.length > 0 ? Math.round(post.dataValues.PostReviews.reduce((acc, cur) => acc + cur.dataValues.rating * 2, 0) / post.dataValues.PostReviews.length) / 2 : 0,
+		};
+	});
+
+	return posts;
+}
+
+async function findHotPosts(userId) {
+	const likes = await PostLike.findAll({
+		where: { createdAt: { [Op.gt]: new Date(Date.now() - 30 * 24 * 60 * 60 * 1000) } },
+		attributes: [[PostLike.sequelize.fn("COUNT", PostLike.sequelize.col("postId")), "count"]],
+		group: [col("Post.id")],
+		order: [[PostLike.sequelize.fn("COUNT", col("Post.id")), "DESC"]],
+		limit: 20,
+		include: [{ model: Post }],
+	}).catch(err => {
+		console.log(err);
+		return null;
+	});
+	if (!likes) return null;
+
+	const posts = likes.map(like => like.dataValues.Post.dataValues);
+
+	const formattedPosts = await Promise.all(posts.map(async postLean => {
+		const post = await findPostById(postLean.id, userId);
+		return post;
+	}));
+
+	return formattedPosts.filter(post => post !== null);
+}
+
+async function findNewPosts(userId) {
+	return await findPosts(userId, {
+		order: [["createdAt", "DESC"]],
+		limit: 20,
+	});
+}
+
+async function findCuratedPosts(userId) {
+	// Get 20 random posts from the last 100 posts
+	return await findPosts(userId, {
+		order: [["createdAt", "DESC"]],
+		limit: 100,
+	});
+}
+
+async function findBreakfastPosts(userId) {
+	return await findPosts(userId, {
+		where: { category: "Breakfast" },
+		order: [["createdAt", "DESC"]],
+		limit: 20,
+	});
+}
+
+async function findLunchPosts(userId) {
+	return await findPosts(userId, {
+		where: { category: "Lunch" },
+		order: [["createdAt", "DESC"]],
+		limit: 20,
+	});
+}
+
+async function findDinnerPosts(userId) {
+	return await findPosts(userId, {
+		where: { category: "Dinner" },
+		order: [["createdAt", "DESC"]],
+		limit: 20,
+	});
+}
+
+async function findDessertPosts(userId) {
+	return await findPosts(userId, {
+		where: { category: "Dessert" },
+		order: [["createdAt", "DESC"]],
+		limit: 20,
+	});
 }
 
 router.get("/", authenticateToken, async (req, res) => {
+	const { userId } = req.body;
+	const [
+		hot,
+		newPosts,
+		curated,
+		breakfast,
+		lunch,
+		dinner,
+		dessert,
+	] = await Promise.all([
+		findHotPosts(userId).catch(() => []),
+		findNewPosts(userId).catch(() => []),
+		findCuratedPosts(userId).catch(() => []),
+		findBreakfastPosts(userId).catch(() => []),
+		findLunchPosts(userId).catch(() => []),
+		findDinnerPosts(userId).catch(() => []),
+		findDessertPosts(userId).catch(() => []),
+	]);
 
-	const numberOfRecipesToFetch = 20;
-	const maxId = 2140392;
-	const randomIds = generateRandomIds(numberOfRecipesToFetch, maxId);
+	const data = {
+		hot,
+		new: newPosts,
+		curated,
+		breakfast,
+		lunch,
+		dinner,
+		dessert,
+	};
 
-	const random = await Recipe.findAll({
-		where: {
-			id: {
-				[Op.in]: randomIds,
-			},
-		},
-	});
-	const picks = random.map(rec => {
-		const { id, title } = rec.dataValues;
-		return {
-			recipeId: id,
-			title,
-			caption: "Give this a try!",
-			username: "Reciped",
-			rating: 3,
-			category: "All",
-		};
-	});
-
-	const userPosts = await Post.findAll({
-		include: [{ model: User }, { model: Recipe }, { model: PostReview }],
-	});
-
-	const posts = userPosts.map((post) => {
-		const { id, recipeId, caption, category, createdAt } = post.dataValues;
-		const reviews = post.dataValues.PostReviews;
-		const ratings = reviews.map((rev) => rev.dataValues.rating);
-		const sum = ratings.reduce(
-			(accumulator, currentValue) => accumulator + currentValue,
-			0,
-		);
-		const rating = sum / ratings.length;
-		const username = post.dataValues.User.dataValues.username;
-		const title = post.dataValues.Recipe.dataValues.title;
-		return {
-			id,
-			recipeId,
-			caption,
-			category,
-			username,
-			title,
-			createdAt,
-			rating,
-		};
-	});
-
-	res.send({ posts, picks });
+	res.send(data);
 });
 
 export default router;
