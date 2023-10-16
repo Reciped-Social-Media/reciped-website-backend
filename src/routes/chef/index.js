683a8b1df84e61983e34c423393098e40c30efca
Xirynx <64374129+Xirynx@users.noreply.github.com>
2023-10-17 00:43:23 +0200

    Add placeholder chef endpoints

--- src/routes/chef/index.js
+++ src/routes/chef/index.js
@@ -1,7 +1,31 @@
 import express from "express";
 import recommend from "./recommend.js";
+import { authenticateToken } from "../../middleware/index.js";
+import { Recipe, UserIngredient, sequelize } from "../../database/index.js";
 
 const router = express.Router();
+
+router.get("/", authenticateToken, async (req, res) => {
+	const { userId } = req.body;
+
+	const ingredients = await UserIngredient.findAll({ where: { userId } }).catch((err) => {
+		console.log(err);
+		res.status(500).send({ error: "Something went wrong" });
+		return;
+	});
+	if (!ingredients) return;
+
+	// TODO: Get recipe recommendations based on ingredients in pantry
+	// Placeholder - Get 20 random recipes
+	const recipes = await Recipe.findAll({ order: sequelize.random(), limit: 5 }).catch((err) => {
+		console.log(err);
+		res.status(500).send({ error: "Something went wrong" });
+		return;
+	});
+	if (!recipes) return;
+
+	res.send(recipes.map(recipe => { return { title: recipe.dataValues.title, ingredients: recipe.dataValues.ingredients, directions: recipe.dataValues.directions }; }));
+});
 
 router.use("/recommend", recommend);
 
--- src/routes/chef/recommend.js
+++ src/routes/chef/recommend.js
@@ -4,26 +4,9 @@ import { UserIngredient, Recipe, sequelize } from "../../database/index.js";
 
 const router = express.Router();
 
-router.get("/", authenticateToken, async (req, res) => {
-	const { userId } = req.body;
-
-	const ingredients = await UserIngredient.findAll({ where: { userId } }).catch((err) => {
-		console.log(err);
-		res.status(500).send({ error: "Something went wrong" });
-		return;
-	});
-	if (!ingredients) return;
-
-	// TODO: Get recipe recommendations based on ingredients in pantry
-	// Placeholder - Get 20 random recipes
-	const recipes = await Recipe.findAll({ order: sequelize.random(), limit: 20 }).catch((err) => {
-		console.log(err);
-		res.status(500).send({ error: "Something went wrong" });
-		return;
-	});
-	if (!recipes) return;
-
-	res.send(recipes);
+router.post("/", authenticateToken, async (req, res) => {
+	const { userId, ingredients } = req.body;
+	// Make recommendations based on ingredients given
 });
 
 export default router;
