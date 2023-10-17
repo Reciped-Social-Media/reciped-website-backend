8d3b1040c4424c1afe88ffe6de38090515e2a9db
JoshuaRuthel <jruthel@syftanalytics.com>
2023-10-18 00:06:58 +0200

    Pantry fix

--- src/routes/pantry/add.js
+++ src/routes/pantry/add.js
@@ -6,18 +6,20 @@ const router = express.Router();
 
 router.post("/", authenticateToken, async (req, res) => {
 	const { userId, ingredientId, storage, unit, amount } = req.body;
+	console.log(userId, ingredientId, storage, unit, amount);
 
 	if (
 		!ingredientId || typeof ingredientId !== "number" ||
 		!storage || typeof storage !== "string" ||
 		!unit || typeof unit !== "string" ||
-		!amount || typeof amount !== "number"
+		!amount
 	) {
+		console.log("here");
 		res.status(400).send("Invalid format");
 		return;
 	}
 
-	const ingredient = await UserIngredient.findOrCreate({ userId, ingredientId, storage, unit, amount })
+	const ingredient = await UserIngredient.findOrCreate({ where: { userId, ingredientId, storage, unit, amount } })
 		.catch((err) => {
 			console.log(err);
 			res.status(500).send("Something went wrong!");
