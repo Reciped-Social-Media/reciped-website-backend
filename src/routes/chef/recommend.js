129a9c1e3a4d1918d1f734dea0a6291a6ab371c7
Xirynx <64374129+Xirynx@users.noreply.github.com>
2023-10-18 22:45:46 +0200

    Complete recommendations route

--- src/routes/chef/recommend.js
+++ src/routes/chef/recommend.js
@@ -1,12 +1,51 @@
 import express from "express";
 import { authenticateToken } from "../../middleware/index.js";
-import { UserIngredient, Recipe, sequelize } from "../../database/index.js";
+import { Recipe } from "../../database/index.js";
+import fetch from "node-fetch";
+import { Op } from "sequelize";
 
 const router = express.Router();
 
 router.post("/", authenticateToken, async (req, res) => {
-	const { userId, ingredients } = req.body;
-	// Make recommendations based on ingredients given
+	const { ingredients } = req.body;
+
+	if (!ingredients || !Array.isArray(ingredients) || ingredients.length === 0 || ingredients.some(ingredient => typeof ingredient !== "string")) {
+		return res.status(400).json({ error: "Invalid format" });
+	}
+
+	let recommendedIds;
+	fetch("http://localhost:105/recommend", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			chef: true,
+			input: ingredients,
+		}),
+	})
+		.then(response => response.json())
+		.then(json => recommendedIds = json)
+		.catch(() => recommendedIds = undefined);
+
+	if (!recommendedIds) {
+		return res.status(500).json({ error: "Something went wrong!" });
+	}
+
+	const recommendedRecipes = await Recipe.findAll({
+		where: {
+			[Op.in]: recommendedIds,
+		},
+	});
+
+	res.send(recommendedRecipes.map(recipe => {
+		return {
+			id: recipe.id,
+			title: recipe.title,
+			ingredients: recipe.ingredients,
+			directions: recipe.directions,
+		};
+	}));
 });
 
 export default router;
